a7e06dd5f7f1d77321e5de983f4758e53733e002
pocandroid <62979933+pocandroid@users.noreply.github.com>
2020-04-22 13:28:29 +0530

    Update SecondActivity.java

--- app/src/main/java/com/sample/pocapplication/SecondActivity.java
+++ app/src/main/java/com/sample/pocapplication/SecondActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Button;
+
 
 public class SecondActivity extends AppCompatActivity {
 
     private TextView textView;
+    private Button btnPPT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +21,19 @@ public class SecondActivity extends AppCompatActivity {
 
         textView=(TextView)findViewById(R.id.textView);
         textView.setTextColor(Color.BLUE);
+        btnPPT=(Button)findViewById(R.id.btnPPT);
+
+        btnPPT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateNew();
+            }
+        });
     }
+
+    public void validateNew(){
+        Intent in=new Intent(this,ThirdActivity.class);
+        startActivity(in);
+    }
+
 }
